d259c7949bca206c26c7758159997f0186d1599c
RezenkovD <rezenkovdmitro@gmail.com>
2024-01-11 22:08:14 +0200

    feat: add models Person, Team and Membership

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Team, Person, Membership
+
 
 # Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email"]
+
+
+@admin.register(Membership)
+class MembershipAdmin(admin.ModelAdmin):
+    list_display = ["person", "team"]
--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Team(models.Model):
+    title = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.title
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Membership(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.person} is a member of {self.team}"
